fcfc167482f207f9d7ff8393ad54fbc52281a2d3
pyzhou <pyzhou@talend.com>
2024-01-03 15:04:43 +0800

    chore(TDI-50747):Happy New Year (#2375)

--- examples/components-api-archetypes/input-component-archetype/src/main/resources/archetype-resources/__rootArtifactId__-definition/src/main/java/StringDelimiter.java
+++ examples/components-api-archetypes/input-component-archetype/src/main/resources/archetype-resources/__rootArtifactId__-definition/src/main/java/StringDelimiter.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2023 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2024 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%${symbol_escape}features${symbol_escape}org.talend.rcp.branding.%PRODUCTNAME%${symbol_escape}%PRODUCTNAME%license.txt
